3939b5db0818b69b532d679ff8dd37e054587643
D4ZO <120313312+D4vendetta@users.noreply.github.com>
2023-02-13 17:46:58 +0900

    [Silver III] Title: 주유소, Time: 84 ms, Memory: 42684 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -7,21 +7,24 @@ distance = list(map(int,sys.stdin.readline().split()))
 price = list(map(int,sys.stdin.readline().split()))
 
 min_cost = min(price)
-better_cost = 0
 j = -1
 
 for i in range(n-1) :
-    if i <= j : 
+    if i < j : 
         continue
     if price[i] == min_cost :
         cost += sum(distance[i:]) * price[i]
         break
 
+    if i == n-2 : 
+        cost += price[i]*price[i-1]
+        break
+
     elif price[i] < price[i+1] :
-        for j in range(i+1, n-1) :
+        for j in range(i+1, n) :
             if price[i] > price[j] :
                 break
-        cost += price[i] * sum(distance[i:j+1])
+        cost += price[i] * sum(distance[i:j])
 
     elif price[i] >= price[i+1] :
         cost += price[i]*distance[i]
