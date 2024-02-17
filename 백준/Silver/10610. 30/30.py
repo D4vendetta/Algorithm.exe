afb79afaea9789177f2b0b2920c5a011d4109863
D4ZO <bangcock21@naver.com>
2024-02-16 23:13:57 -0800

    [Silver IV] Title: 30, Time: 80 ms, Memory: 32252 KB -BaekjoonHub

---
+++ 백준/Silver/10610. 30/30.py
@@ -0,0 +1,15 @@
+import sys
+
+n = sys.stdin.readline().rstrip('\n')
+n = sorted(n, reverse=True)
+sum = 0
+
+if '0' not in n:
+  print(-1)
+else:
+  for i in n:
+      sum += int(i)
+  if sum % 3 != 0 :	# 3의 배수 체크
+      print(-1)
+  else :
+      print(''.join(n))
